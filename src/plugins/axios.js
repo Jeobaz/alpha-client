199f093380e60d2186cf57f57526e79104fef4df
Кирилл Манов <kirill.manov@list.ru>
2021-06-02 16:53:24 +0500

    env api_url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,12 +4,12 @@ import Vue from 'vue';
 import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: "http://127.0.0.1:3333/api/",
+  //baseURL: "http://127.0.0.1:3333/api/",
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
